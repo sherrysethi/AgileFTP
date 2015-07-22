736927792fae6f2df86e78cd15281e5ded8f189d
mgarud <mgarud@gmail.com>
2015-07-21 17:08:00 -0700

    log in and load server address
    added log in and password command line , and taking server address from
    user

--- src/FTPmain.java
+++ src/FTPmain.java
@@ -1,12 +1,35 @@
+import java.util.Scanner;
+
 
 public class FTPmain {
+
+	private static String Username = "test";
+	private static String Password = "test";
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	    System.out.println("Enter Username: ");
+	    Scanner uName = new Scanner(System.in);
+	    String UN = uName.nextLine();
 
+	    System.out.println("Enter Password: ");
+	    Scanner password = new Scanner(System.in);
+	    String PW = password.nextLine();
+
+	    if (UN.equals(Username) && PW.equals(Password))
+	    {
+	        System.out.println("Login Successful !");
+	    }
+	    else {
+	        System.out.println("Please try again.");
+	    }
+	    
+	    System.out.println("Enter server address: ");
+	    Scanner serverAdd = new Scanner(System.in);
+	    String serverAddStr = serverAdd.nextLine();
+	    
 	}
 
 }
