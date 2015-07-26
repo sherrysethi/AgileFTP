bfa000068b53f8df973ec4d606db428690fcc658
dbawale <dbawale@pdx.edu>
2015-07-26 01:21:17 -0700

    Changelog
    Hi Team,
    
    I've made a new branch called listdirbranch in the repo and made the
    following changes:
    
    1. Implemented command line interface that outputs "ftp>>" and listens
    for command from user, taking appropriate actions.
    
    2. Implemented user log in to FileZilla server. I've preserved the
    'test' login on my machine. I also tested it with anonymous login on
    Edinburgh server.
    
    3. Implemented list files and directories functionality. Directories are
    marked with ~ and files are marked with *
    
    4. Implemented 'help' functionality as a part of documentation process.
    When user types 'help' on the command line, he will be shown a list of
    available commands. This is still under construction and should be
    updated as the project proceeds.
    
    5. Implemented log out and exit functionality. This was not a task, but
    it was necessary to log out of the server before exiting the code. It
    can and should be separated later and documentation should be updated
    accordingly.
    
    I'm waiting for test cases from Abhishek so that I can execute them, as
    per our planned tasks, for the first story. It will be good if we can
    maintain all test cases in a shared Excel on drive along with the
    results. I will add the test cases for the second story when the file is
    shared.
    
    An issue that remains to be resolved is handling server response to both
    login and list files. I'll work on both tomorrow.
    
    Another feature we need to implement is password masking, where a series
    of * will be displayed to the user while typing the password, instead of
    the plain text that we currently have.
    
    Let's target finishing the first two stories completely by Tuesday, at
    the very least.
    
    Please test this code whenever you guys have time and give me feedback
    :)
    
    Deven

--- src/FTPmain.java
+++ src/FTPmain.java
@@ -1,35 +1,136 @@
+import java.io.IOException;
 import java.util.Scanner;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
 
-
-public class FTPmain {
+public class FTPmain //main class for FTP Client
+{
 
 	private static String Username = "test";
 	private static String Password = "test";
+	public static FTPClient client; //client is an object of type FTPClient
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		client = new FTPClient(); //initialize client
+		
+		//get username from user
 	    System.out.println("Enter Username: ");
 	    Scanner uName = new Scanner(System.in);
 	    String UN = uName.nextLine();
 
+	    //get password from user
 	    System.out.println("Enter Password: ");
 	    Scanner password = new Scanner(System.in);
 	    String PW = password.nextLine();
-
-	    if (UN.equals(Username) && PW.equals(Password))
-	    {
-	        System.out.println("Login Successful !");
-	    }
-	    else {
-	        System.out.println("Please try again.");
-	    }
 	    
+	    //get server address from user
 	    System.out.println("Enter server address: ");
 	    Scanner serverAdd = new Scanner(System.in);
 	    String serverAddStr = serverAdd.nextLine();
 	    
+	    //check if login successful, handle response from server
+	    if(FTPmain.ftplogin(UN, PW, serverAddStr)==false)
+	    {
+	    	return;
+	    }
+	    
+	    //call function which will implement command line interface
+	    FTPmain.ftplooper();
 	}
-
+	
+	public static void ftplooper()
+	{
+		while(true)
+	    {
+	    	System.out.println("ftp>>"); //display the command line
+	    	Scanner inputread = new Scanner(System.in); //read command from user
+	    	String inputCommand = inputread.nextLine();
+	    	
+	    	if(inputCommand.equals("help")) //user asks for help, display documentation
+	    	{
+	    		FTPmain.ftpdisplayhelp();
+	    	}
+	    	
+	    	else if(inputCommand.equals("ls")) //list all files and directories
+	    	{
+	    		FTPmain.ftplistfiles();
+	    	}
+	    	
+	    	else if(inputCommand.equals("exit")) //log out from client
+	    	{
+	    		try
+	    		{
+	    			if(client.logout()==true)
+	    			{
+	    				System.out.println("Logged out from client");
+	    				break;
+	    			}
+	    			else
+	    			{
+	    				System.out.println("Error logging out of client. Could not exit.");
+	    			}
+	    		}
+	    		catch(IOException e)
+	    		{
+	    			e.printStackTrace();
+	    		}
+	    	}
+	    	else
+	    	{
+	    		System.out.println("Bad Command");
+	    	}
+	    }
+	}
+	public static boolean ftplogin(String uname, String pwd, String address)
+	{
+		try
+		{
+			client.connect(address, 21); //connect to the address provided by the user 
+			if(client.login(uname, pwd))
+			{
+				return(true);
+			}
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public static void ftpdisplayhelp()
+	{
+		//Display help messages
+		System.out.println("Welcome to the Dexters FTP Server!");
+		System.out.println("The following commands are available:");
+		System.out.println("1. ls: List all directories and files. Directories are marked with ~ and files are marked with *");
+		System.out.println("2. exit: Log out of the current session and exit the program.");
+		System.out.println("3. help: Display this help message.");
+		System.out.println("End of help text");
+	}
+	
+	public static void ftplistfiles()
+	{
+		try
+		{
+			FTPFile[] files = client.listFiles(); //get list of files from server and store it in array of type FTPFile
+			for(FTPFile file : files)//for each stored entry
+			{
+				if(file.isDirectory()) //check if the current entry is a directory
+				{
+					System.out.println("~ " + file.getName()); //display with ~
+				}
+				else
+				{
+					System.out.println("* " + file.getName()); //if file display with *
+				}
+			}
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
 }
